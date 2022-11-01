948f00bc623e47252420c79cc61ab822dd5da6f1
Jhojan Martínez <jhojanmartinez04@hotmail.com>
2022-10-31 22:51:42 -0500

    Added: design ended

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class resultados(forms.Form):
-    array = forms.CharField(label="Inserte la lista", required=True)
+    array = forms.CharField(label="Inserte la lista", required=False)
+    file = forms.FileField(label="Anexe archivo", required=False)
--- myapp/views.py
+++ myapp/views.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 from collections import Counter
 import numpy as np
-from .forms import resultados 
+from .forms import resultados
+import pandas as pd
+import openpyxl 
 # Create your views here.
 
 
 def hello(request, username):
     return HttpResponse(username)
+
 
 def distriNormal(lista):
 
@@ -21,64 +24,73 @@ def distriNormal(lista):
     z1 = (x1-mu)/sigma
     z2 = (x2-mu)/sigma
 
-    x = np.arange(z1,z2,0.0001)
-    x2 = np.arange(-10,10,0.0001)
-    y = norm.pdf(x,0,1)
-    y2 = norm.pdf(x2,0,1)
-    fig,ax = plt.subplots(figsize=(9,6))
+    x = np.arange(z1, z2, 0.0001)
+    x2 = np.arange(-10, 10, 0.0001)
+    y = norm.pdf(x, 0, 1)
+    y2 = norm.pdf(x2, 0, 1)
+    fig, ax = plt.subplots(figsize=(9, 6))
     plt.style.use("fivethirtyeight")
-    ax.plot(x2,y2)
-    ax.fill_between(x,y,0,color="g")
-    ax.fill_between(x2,y2,0,alpha=0.1)
+    ax.plot(x2, y2)
+    ax.fill_between(x, y, 0, color="g")
+    ax.fill_between(x2, y2, 0, alpha=0.1)
     ax.set_xlabel("numero de elementos fuera del promedio")
     ax.set_title("Distribucion normal")
     plt.savefig("gaussiana.png")
+
 
 def entero(x):
     return parseInt(x)
 
 
 def index(request):
-    ### = [1,2,2,25,3,2,4,5,2,5,6,7,3,5,2,4,5,6,7,4,20,30,43,22,19,3]
+    # = [1,2,2,25,3,2,4,5,2,5,6,7,3,5,2,4,5,6,7,4,20,30,43,22,19,3]
+    lista = []
+    if (request.GET['array'] == '' and request.GET['file'] != ''):
+        print("entre")
+        read = pd.read_csv(request.GET['file'])
+        read = read.values
+        print(len(read))
+        lista = read
+    elif (request.GET['array'] != ''):
+        lista = request.GET['array']
+        lista = lista[1:]
+        lista = lista[:-1]
+        lista = lista.split(',')
 
-    lista = request.GET['array']
-    lista = lista[1:]
-    lista = lista[:-1]
-    lista = lista.split(',')
+        lista = [int(x) for x in lista]
 
-    lista = [int(x) for x in lista]
+        print(lista)
 
-    print(lista)
-    
-    media = np.mean(lista)
-    mediana = np.median(lista)
-    desviacion = np.std(lista)
-    varianza = np.var(lista)
-    minimo = min(lista)
-    maximo = max(lista)
-    counter = Counter(lista)
-    moda = counter.most_common()
-    moda2 = moda[0]
-    distriNormal(lista)
+        media = np.mean(lista)
+        mediana = np.median(lista)
+        desviacion = np.std(lista)
+        varianza = np.var(lista)
+        minimo = min(lista)
+        maximo = max(lista)
+        counter = Counter(lista)
+        moda = counter.most_common()
+        moda2 = moda[0]
+        distriNormal(lista)
 
-    return render(request,'index.html',{
-        'mediana': mediana,
-        'media': media,
-        'desviacion': desviacion,
-        'varianza': varianza,
-        'min': minimo,
-        'max': maximo,
-        'moda': moda2,
-    })
+        return render(request, 'index.html', {
+            'mediana': mediana,
+            'media': media,
+            'desviacion': desviacion,
+            'varianza': varianza,
+            'min': minimo,
+            'max': maximo,
+            'moda': moda2,
+        })
+    else:
+        return HttpResponse("404")
 
 
 def about(request):
     return HttpResponse("About")
 
 
-
 def input(request):
 
-    return render(request,'input.html', {
+    return render(request, 'input.html', {
         'form': resultados
     })
